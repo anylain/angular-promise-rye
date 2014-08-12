adbe3a842c0f18095c04b61d59f48bf2e933b105
PanYing <anylain@gmail.com>
2014-08-12 15:14:27 +0800

    * change version to 0.2.0

--- src/angular-rye.js
+++ src/angular-rye.js
@@ -1,5 +1,5 @@
 /**
- * Angular promise rye v0.2.0-pre https://github.com/anylain/angular-promise-rye
+ * Angular promise rye v0.2.0 https://github.com/anylain/angular-promise-rye
  * -----------------------------------------------------------------------------
  * Copyright 2014 PanYing <anylain@gmail.com> Released under the MIT License
  */
