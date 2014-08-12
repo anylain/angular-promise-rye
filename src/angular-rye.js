a860c88612a214a2bf1ad6d49d90a0b9b61993cf
PanYing <anylain@gmail.com>
2014-08-12 14:56:06 +0800

    * 使用闭包重构

--- src/angular-rye.js
+++ src/angular-rye.js
@@ -1,131 +1,144 @@
-'use strict';
 /**
- * Created by Soul on 2014/8/9.
+ * Angular promise rye v0.2.0-pre https://github.com/anylain/angular-promise-rye
+ * -----------------------------------------------------------------------------
+ * Copyright 2014 PanYing <anylain@gmail.com> Released under the MIT License
  */
 
-angular.module('ngPromiseRye', []).
+(function(window, angular, undefined) {
+  'use strict';
 
-  factory('QUtil', function() {
-    var tryGetFun = function(fun, promise) {
-      if (!promise) {
-        return;
-      }
-      if (promise.denodeify) {
-        return $q.when(promise)[fun];
-      }
-      return promise[fun] || promise['$' + fun] ||
-        (promise.$promise && promise.$promise[fun]);
-    };
+  var tryGetFun = function(fun, promise) {
+    if (!promise) {
+      return;
+    }
+    if (promise.denodeify) {
+      return $q.when(promise)[fun];
+    }
+    return promise[fun] || promise['$' + fun] ||
+      (promise.$promise && promise.$promise[fun]);
+  };
 
-    return {
-      getThen: function(promise) {
-        return tryGetFun('then', promise) || angular.noop;
-      },
-      onFinally: function(promise, callback) {
-        (tryGetFun('then', promise) || angular.noop)(callback, callback);
-      },
-      handler: function(callback) {
-        return function(promise) {
-          if (promise && !angular.isString(promise)) {
-            callback(promise);
-          }
+  var getThen = function(promise) {
+    return tryGetFun('then', promise) || angular.noop;
+  };
+
+
+  var onFinally = function(promise, callback) {
+    (tryGetFun('then', promise) || angular.noop)(callback, callback);
+  };
+
+  var handler = function(callback) {
+    return function(promise) {
+      if (promise && !angular.isString(promise)) {
+        callback(promise);
+      }
+    }
+  };
+
+  function disableElement(element, isDisabled) {
+    var ignore = element.attr('rye-ignore') || element.attr('ryeIgnore');
+    if (ignore !== undefined && ignore !== 'false') {
+      return;
+    }
+    var tag = element[0].tagName;
+    if (tag === 'INPUT' || tag === 'BUTTON') {
+      element.prop({'disabled': isDisabled});
+    } else {
+      element[(isDisabled ? 'addClass' : 'removeClass')]('disabled');
+      angular.forEach(element.children(), function(child) {
+        disableElement(angular.element(child), isDisabled);
+      });
+    }
+  }
+
+  function hide(element) {
+    element.css('display', 'none');
+  }
+
+  function show(element) {
+    element.css('display', '');
+  }
+
+  angular.module('ngPromiseRye', []).
+
+    directive('ryeShowOnFault', function() {
+
+      return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+
+          scope.$watchCollection(attrs.ryeShowOnFault, function(promise) {
+
+            hide(element);
+
+            getThen(promise)(null, function(reason) {
+              scope.faultReason = reason;
+              show(element);
+            });
+          }, true);
+
         }
-      }
-    };
-  }).
+      };
+    }).
 
-  directive('ryeShowOnFault', ['QUtil', function(QUtil) {
+    directive('ryeShowInProcess', function(QUtil) {
 
-    return {
-      restrict: 'A',
-      link: function(scope, element, attrs) {
+      return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
 
-        scope.$watchCollection(attrs.ryeShowOnFault, function(promise) {
+          hide(element);
 
-          element.css('display', 'none');
+          scope.$watchCollection(attrs.ryeShowInProcess, handler(function(promise) {
 
-          QUtil.getThen(promise)(null, function(reason) {
-            scope.faultReason = reason;
-            element.css('display', '');
-          });
-        }, true);
+            show(element);
 
-      }
-    };
-  }]).
+            onFinally(promise, function() {
+              hide(element);
+            });
+          }), true);
 
-  directive('ryeShowInProcess', ['QUtil', function(QUtil) {
+        }
+      };
+    }).
 
-    return {
-      restrict: 'A',
-      link: function(scope, element, attrs) {
+    directive('ryeHideInProcess', function(QUtil) {
 
-        element.css('display', 'none');
+      return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
 
-        scope.$watchCollection(attrs.ryeShowInProcess, QUtil.handler(function(promise) {
+          scope.$watchCollection(attrs.ryeHideInProcess, handler(function(promise) {
 
-          element.css('display', '');
+            hide(element);
 
-          QUtil.onFinally(promise, function() {
-            element.css('display', 'none');
-          });
-        }), true);
+            onFinally(promise, function() {
+              show(element);
+            });
+          }), true);
 
-      }
-    };
-  }]).
+        }
+      };
+    }).
 
-  directive('ryeHideInProcess', ['QUtil', function(QUtil) {
+    directive('ryeDisableInProcess', function(QUtil) {
 
-    return {
-      restrict: 'A',
-      link: function(scope, element, attrs) {
+      return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
 
-        scope.$watchCollection(attrs.ryeHideInProcess, QUtil.handler(function(promise) {
+          scope.$watchCollection(attrs.ryeDisableInProcess, handler(function(promise) {
 
-          element.css('display', 'none');
+            disableElement(element, true);
 
-          QUtil.onFinally(promise, function() {
-            element.css('display', '');
-          });
-        }), true);
+            onFinally(promise, function() {
+              disableElement(element, false);
+            });
 
-      }
-    };
-  }]).
+          }), true);
 
-  directive('ryeDisableInProcess', ['QUtil', function(QUtil) {
+        }
+      };
+    });
 
-    function disableElement(element, isDisabled) {
-      var ignore = element.attr('rye-ignore') || element.attr('ryeIgnore');
-      if (ignore !== undefined && ignore !== 'false') {
-        return;
-      }
-      var tag = element[0].tagName;
-      if (tag === 'INPUT' || tag === 'BUTTON') {
-        element.prop({'disabled': isDisabled});
-      } else {
-        element[(isDisabled ? 'addClass' : 'removeClass')]('disabled');
-        angular.forEach(element.children(), function(child) {
-          disableElement(angular.element(child), isDisabled);
-        });
-      }
-    };
-
-    return {
-      restrict: 'A',
-      link: function(scope, element, attrs) {
-
-        scope.$watchCollection(attrs.ryeDisableInProcess, QUtil.handler(function(promise) {
-
-          disableElement(element, true);
-
-          QUtil.onFinally(promise, function() {
-            disableElement(element, false);
-          });
-
-        }), true);
-
-      }
-    };
-  }]);
+})(window, window.angular);
