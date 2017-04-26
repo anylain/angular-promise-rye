a227263b88262bb9cf55a1a8969408b93505c0d1
Panying <anylain@gmail.com>
2017-04-26 18:04:54 +0800

    fix promise match bug

--- src/angular-rye.js
+++ src/angular-rye.js
@@ -1,16 +1,22 @@
 /**
- * Angular promise rye v0.4.2 https://github.com/anylain/angular-promise-rye
+ * Angular promise rye v0.4.3 https://github.com/anylain/angular-promise-rye
  * -----------------------------------------------------------------------------
  * Copyright 2014 PanYing <anylain@gmail.com> Released under the MIT License
  */
 
-(function(window, angular, undefined) {
+(function (window, angular, undefined) {
   'use strict';
 
 
   function callThen(promise, onFulfilled, onRejected) {
     if (promise) {
-      (promise.then || promise.$then).call(promise, onFulfilled, onRejected);
+      var onFulfilledCallback = onFulfilled ? function () {
+        onFulfilled(promise);
+      } : null;
+      var onRejectedCallback = onRejected ? function (reason) {
+        onRejected(promise, reason);
+      } : null;
+      (promise.then || promise.$then).call(promise, onFulfilledCallback, onRejectedCallback);
     }
   }
 
@@ -43,7 +49,7 @@
       element.prop({'disabled': isDisabled});
     } else {
       element[(isDisabled ? 'addClass' : 'removeClass')]('disabled');
-      angular.forEach(element.children(), function(child) {
+      angular.forEach(element.children(), function (child) {
         disableElement(angular.element(child), isDisabled);
       });
     }
@@ -57,230 +63,214 @@
     element.css('display', '');
   }
 
-  angular.module('ngPromiseRye', []).
+  angular.module('ngPromiseRye', []).directive('ryeShowOnFault', ['$parse', function ($parse) {
 
-    directive('ryeShowOnFault', ['$parse', function($parse) {
+    return {
+      restrict: 'A',
+      link: function (scope, element, attrs) {
 
-      return {
-        restrict: 'A',
-        link: function(scope, element, attrs) {
+        scope.$watchCollection($parse(attrs.ryeShowOnFault, watchInterceptor), function (promise) {
 
-          scope.$watchCollection($parse(attrs.ryeShowOnFault, watchInterceptor), function(promise) {
-
-            scope.currPromise = promise;
-
-            hide(element);
-
-            callThen(promise, null, function(reason) {
-              if (scope.currPromise === promise) {
-                scope.faultReason = reason;
-                show(element);
-              }
-            });
-          });
-
-        }
-      };
-    }]).
-
-    directive('ryeShowOnSuccess', ['$parse', function($parse) {
-
-      return {
-        restrict: 'A',
-        link: function(scope, element, attrs) {
-
-          scope.$watchCollection($parse(attrs.ryeShowOnSuccess, watchInterceptor), function(promise) {
-
-            scope.currPromise = promise;
-
-            hide(element);
-
-            callThen(promise, function() {
-              if (scope.currPromise === promise) {
-                show(element);
-              }
-            });
-          });
-
-        }
-      };
-    }]).
-
-    directive('ryeHideOnFault', ['$parse', function($parse) {
-
-      return {
-        restrict: 'A',
-        link: function(scope, element, attrs) {
-
-          scope.$watchCollection($parse(attrs.ryeHideOnFault, watchInterceptor), function(promise) {
-
-            scope.currPromise = promise;
-
-            show(element);
-
-            callThen(promise, null, function() {
-              if (scope.currPromise === promise) {
-                hide(element);
-              }
-            });
-          });
-
-        }
-      };
-    }]).
-
-    directive('ryeHideOnSuccess', ['$parse', function($parse) {
-
-      return {
-        restrict: 'A',
-        link: function(scope, element, attrs) {
-
-          scope.$watchCollection($parse(attrs.ryeHideOnSuccess, watchInterceptor), function(promise) {
-
-            scope.currPromise = promise;
-
-            show(element);
-
-            callThen(promise, function() {
-              if (scope.currPromise === promise) {
-                hide(element);
-              }
-            });
-          });
-
-        }
-      };
-    }]).
-
-    directive('ryeShowInProcess', ['$parse', function($parse) {
-
-      return {
-        restrict: 'A',
-        link: function(scope, element, attrs) {
+          var currPromise = promise;
 
           hide(element);
 
-          scope.$watchCollection($parse(attrs.ryeShowInProcess, watchInterceptor), function(promise) {
-
-            scope.currPromise = promise;
-
-            if (promise && !angular.isString(promise)) {
-
-              show(element);
-
-              onFinally(promise, function() {
-                if (scope.currPromise === promise) {
-                  hide(element);
-                }
-              });
-            } else {
-              hide(element);
-            }
-          });
-
-        }
-      };
-    }]).
-
-    directive('ryeHideInProcess', ['$parse', function($parse) {
-
-      return {
-        restrict: 'A',
-        link: function(scope, element, attrs) {
-
-          scope.$watchCollection($parse(attrs.ryeHideInProcess, watchInterceptor), function(promise) {
-
-            scope.currPromise = promise;
-
-            if (promise && !angular.isString(promise)) {
-
-              hide(element);
-
-              onFinally(promise, function() {
-                if (scope.currPromise === promise) {
-                  show(element);
-                }
-              });
-            } else {
+          callThen(promise, null, function (promise, reason) {
+            if (currPromise === promise) {
+              scope.faultReason = reason;
               show(element);
             }
           });
+        });
 
-        }
-      };
-    }]).
+      }
+    };
+  }]).directive('ryeShowOnSuccess', ['$parse', function ($parse) {
 
-    directive('ryeDisableInProcess', ['$parse', function($parse) {
+    return {
+      restrict: 'A',
+      link: function (scope, element, attrs) {
 
-      return {
-        restrict: 'A',
-        link: function(scope, element, attrs) {
+        scope.$watchCollection($parse(attrs.ryeShowOnSuccess, watchInterceptor), function (promise) {
 
-          scope.$watchCollection($parse(attrs.ryeDisableInProcess, watchInterceptor), function(promise) {
+          var currPromise = promise;
 
-            scope.currPromise = promise;
+          hide(element);
 
-            if (promise && !angular.isString(promise)) {
-
-              disableElement(element, true);
-
-              onFinally(promise, function() {
-                if (scope.currPromise === promise) {
-                  disableElement(element, false);
-                }
-              });
-            } else {
-              disableElement(element, false);
-            }
-
-          });
-        }
-      };
-    }]).
-
-    directive('ryeClass', ['$parse', function($parse) {
-      return {
-        restrict: 'A',
-        link: function(scope, element, attrs) {
-
-          var config = /^(\{.*\})\[(.+)\]$/.exec(attrs.ryeClass);
-
-          var classes = scope.$eval(config[1]);
-          var promiseObj = config[2];
-
-          scope.$watchCollection($parse(promiseObj, watchInterceptor), function(promise) {
-
-            scope.currPromise = promise;
-
-            if (promise && !angular.isString(promise)) {
-
-              element.addClass(classes['process']);
-              element.removeClass([classes.success, classes.fault, classes.finally].join(" "));
-
-              callThen(promise, function() {
-                if (scope.currPromise === promise) {
-                  element.addClass(classes.success);
-                }
-              }, function() {
-                if (scope.currPromise === promise) {
-                  element.addClass(classes.fault);
-                }
-              });
-
-              onFinally(promise, function() {
-                if (scope.currPromise === promise) {
-                  element.removeClass(classes.process);
-                  element.addClass(classes.finally);
-                }
-              });
-            } else {
-              element.removeClass([classes.process, classes.success, classes.fault, classes.finally].join(" "));
+          callThen(promise, function (promise) {
+            if (currPromise === promise) {
+              show(element);
             }
           });
+        });
 
-        }
-      };
-    }]);
+      }
+    };
+  }]).directive('ryeHideOnFault', ['$parse', function ($parse) {
+
+    return {
+      restrict: 'A',
+      link: function (scope, element, attrs) {
+
+        scope.$watchCollection($parse(attrs.ryeHideOnFault, watchInterceptor), function (promise) {
+
+          var currPromise = promise;
+
+          show(element);
+
+          callThen(promise, null, function () {
+            if (currPromise === promise) {
+              hide(element);
+            }
+          });
+        });
+
+      }
+    };
+  }]).directive('ryeHideOnSuccess', ['$parse', function ($parse) {
+
+    return {
+      restrict: 'A',
+      link: function (scope, element, attrs) {
+
+        scope.$watchCollection($parse(attrs.ryeHideOnSuccess, watchInterceptor), function (promise) {
+
+          var currPromise = promise;
+
+          show(element);
+
+          callThen(promise, function (promise) {
+            if (currPromise === promise) {
+              hide(element);
+            }
+          });
+        });
+
+      }
+    };
+  }]).directive('ryeShowInProcess', ['$parse', function ($parse) {
+
+    return {
+      restrict: 'A',
+      link: function (scope, element, attrs) {
+
+        hide(element);
+
+        scope.$watchCollection($parse(attrs.ryeShowInProcess, watchInterceptor), function (promise) {
+
+          if (promise && !angular.isString(promise)) {
+
+            var currPromise = promise;
+
+            show(element);
+
+            onFinally(promise, function (promise) {
+              if (currPromise === promise) {
+                hide(element);
+              }
+            });
+          } else {
+            hide(element);
+          }
+        });
+
+      }
+    };
+  }]).directive('ryeHideInProcess', ['$parse', function ($parse) {
+
+    return {
+      restrict: 'A',
+      link: function (scope, element, attrs) {
+
+        scope.$watchCollection($parse(attrs.ryeHideInProcess, watchInterceptor), function (promise) {
+
+          var currPromise = promise;
+
+          if (promise && !angular.isString(promise)) {
+
+            hide(element);
+
+            onFinally(promise, function (promise) {
+              if (currPromise === promise) {
+                show(element);
+              }
+            });
+          } else {
+            show(element);
+          }
+        });
+
+      }
+    };
+  }]).directive('ryeDisableInProcess', ['$parse', function ($parse) {
+
+    return {
+      restrict: 'A',
+      link: function (scope, element, attrs) {
+
+        scope.$watchCollection($parse(attrs.ryeDisableInProcess, watchInterceptor), function (promise) {
+
+          var currPromise = promise;
+
+          if (promise && !angular.isString(promise)) {
+
+            disableElement(element, true);
+
+            onFinally(promise, function (promise) {
+              if (currPromise === promise) {
+                disableElement(element, false);
+              }
+            });
+          } else {
+            disableElement(element, false);
+          }
+
+        });
+      }
+    };
+  }]).directive('ryeClass', ['$parse', function ($parse) {
+    return {
+      restrict: 'A',
+      link: function (scope, element, attrs) {
+
+        var config = /^(\{.*\})\[(.+)\]$/.exec(attrs.ryeClass);
+
+        var classes = scope.$eval(config[1]);
+        var promiseObj = config[2];
+
+        scope.$watchCollection($parse(promiseObj, watchInterceptor), function (promise) {
+
+          var currPromise = promise;
+
+          if (promise && !angular.isString(promise)) {
+
+            element.addClass(classes['process']);
+            element.removeClass([classes.success, classes.fault, classes.finally].join(" "));
+
+            callThen(promise, function (promise) {
+              if (currPromise === promise) {
+                element.addClass(classes.success);
+              }
+            }, function (promise) {
+              if (currPromise === promise) {
+                element.addClass(classes.fault);
+              }
+            });
+
+            onFinally(promise, function (promise) {
+              if (currPromise === promise) {
+                element.removeClass(classes.process);
+                element.addClass(classes.finally);
+              }
+            });
+          } else {
+            element.removeClass([classes.process, classes.success, classes.fault, classes.finally].join(" "));
+          }
+        });
+
+      }
+    };
+  }]);
 
 })
 (window, window.angular);
